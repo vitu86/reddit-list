df6390d71c89dd6c73102d4cabdaef53ce2baf63
Vitor Costa <vitu86@gmail.com>
2021-01-26 18:27:26 -0300

    [setup] Include gitignore and xcodeproj

---
+++ redditList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  redditList
+//
+//  Created by Vitor Costa on 26/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
